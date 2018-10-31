eb467a345a211de869259f36fb0f3b54ae2281f5
lithial <jameshessell@gmail.com>
2018-11-01 00:05:58 +1300

    Inital Commit. basis of a bot maybe

---
+++ events/message.js
@@ -0,0 +1,17 @@
+module.exports = (client, message) => {
+    //ignore all bots
+    if(message.author.bot) return;
+
+    //ignore messages without prefix
+    if(message.content.indexOf(client.config.prefix)!==0) return;
+
+    // Our standard argument/command name definition.
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    //grab the comand data from the client.commands Enmap
+    const cmd = client.commands.get(command);
+
+    //run the command
+    cmd.run(client,message, args);
+}
