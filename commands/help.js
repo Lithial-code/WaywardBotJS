b97c5f1872c702d131fd51138baebb856b993d5e
lithial <jameshessell@gmail.com>
2018-11-02 23:23:50 +1300

    message

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,21 @@
+const Discord = require("discord.js");
 const config = require("../config.json");
 exports.run = (client, message,args) => {
-    message.channel.send("Available commands are currently condition,ritual, spell and secret. The prefix is currently " + config.prefix).catch(console.error);
+
+    embedMessage();
+
+    //Method for making condition embed
+ function embedMessage(target) {
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setFooter("© Lelantos Studios", client.user.avatarURL)
+      .setTimestamp()
+      .addField("Available Commands: ", "credit, condition, feat, item, ritual, race, roll, spell, secret, support, trait.")
+      .addField("Prefix", config.prefix);
+
+    message.channel.send({ embed });
+  }
 }
+
+
+
---
+++ commands/trait.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+  var fs = require('fs');
+  var json = JSON.parse(fs.readFileSync('./json/traits.json', 'utf8'));
+  //
+  //Make list of all the secret names
+  //
+  if (args[0] == '*') {
+    embedList();
+  }
+  //else write the embed for the json object after pooling all the args into one name.
+  else {
+    var target = "";
+    args.forEach(element => {
+      target += element + " ";
+    });
+    var targettrim = target.trim(); //trim the white space off the end so .includes reads it properlyW
+    var found = false;
+    for (var i = 0; i < json.length; i++) {
+      if (json[i].name.toLowerCase().includes(targettrim)) {
+        found = true;
+        embedMessage(json[i]);
+        break;
+      }
+    }
+  }
+  //
+  //method for making list of names
+  //
+  function embedList() {
+    var list = "";
+    json.forEach(element => {
+      list += element.name + '\n';
+    });
+
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setFooter("© Lelantos Studios", client.user.avatarURL)
+      .setTimestamp()
+      .addField("List: ", list)
+    message.channel.send({ embed });
+  }
+  //Method for making condition embed
+  function embedMessage(target) {
+    const embed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setFooter("© Lelantos Studios", client.user.avatarURL)
+      .setTimestamp()
+      .addField("Name: ", target.name)
+      .addField("Description", target.description);
+
+    message.channel.send({ embed });
+  }
+
+}
